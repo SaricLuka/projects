c512f6c32b9806c80d8b8eabdfe48e050b0c7672
TektiteL <126980334+TektiteL@users.noreply.github.com>
2023-03-10 22:07:46 +0100

    Add files via upload

---
+++ Stopwatch/Stoperica/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Stoperica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ispis = new System.Windows.Forms.Label();
+            this.start = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.reset = new System.Windows.Forms.Button();
+            this.split = new System.Windows.Forms.Button();
+            this.splitIspis = new System.Windows.Forms.RichTextBox();
+            this.save = new System.Windows.Forms.Button();
+            this.saveime = new System.Windows.Forms.TextBox();
+            this.ispisLoads = new System.Windows.Forms.ComboBox();
+            this.load = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ispis
+            // 
+            this.ispis.AutoSize = true;
+            this.ispis.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ispis.Location = new System.Drawing.Point(35, 24);
+            this.ispis.Name = "ispis";
+            this.ispis.Size = new System.Drawing.Size(246, 39);
+            this.ispis.TabIndex = 0;
+            this.ispis.Text = "00:00:0000000";
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(81, 81);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(85, 46);
+            this.start.TabIndex = 1;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // reset
+            // 
+            this.reset.Location = new System.Drawing.Point(172, 81);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(85, 46);
+            this.reset.TabIndex = 2;
+            this.reset.Text = "Reset";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // split
+            // 
+            this.split.Location = new System.Drawing.Point(127, 133);
+            this.split.Name = "split";
+            this.split.Size = new System.Drawing.Size(85, 46);
+            this.split.TabIndex = 3;
+            this.split.Text = "Split";
+            this.split.UseVisualStyleBackColor = true;
+            this.split.Click += new System.EventHandler(this.split_Click);
+            // 
+            // splitIspis
+            // 
+            this.splitIspis.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.splitIspis.Location = new System.Drawing.Point(12, 201);
+            this.splitIspis.Name = "splitIspis";
+            this.splitIspis.ReadOnly = true;
+            this.splitIspis.Size = new System.Drawing.Size(322, 225);
+            this.splitIspis.TabIndex = 4;
+            this.splitIspis.Text = "";
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(12, 484);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(85, 46);
+            this.save.TabIndex = 5;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // saveime
+            // 
+            this.saveime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.saveime.Location = new System.Drawing.Point(12, 447);
+            this.saveime.Name = "saveime";
+            this.saveime.Size = new System.Drawing.Size(138, 26);
+            this.saveime.TabIndex = 6;
+            this.saveime.Text = "Name of the save";
+            this.saveime.Enter += new System.EventHandler(this.saveime_Enter);
+            this.saveime.Leave += new System.EventHandler(this.saveime_Leave);
+            // 
+            // ispisLoads
+            // 
+            this.ispisLoads.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ispisLoads.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ispisLoads.FormattingEnabled = true;
+            this.ispisLoads.Location = new System.Drawing.Point(196, 447);
+            this.ispisLoads.Name = "ispisLoads";
+            this.ispisLoads.Size = new System.Drawing.Size(138, 28);
+            this.ispisLoads.TabIndex = 7;
+            this.ispisLoads.Click += new System.EventHandler(this.ispisLoads_Click);
+            // 
+            // load
+            // 
+            this.load.Location = new System.Drawing.Point(249, 484);
+            this.load.Name = "load";
+            this.load.Size = new System.Drawing.Size(85, 46);
+            this.load.TabIndex = 8;
+            this.load.Text = "Load";
+            this.load.UseVisualStyleBackColor = true;
+            this.load.Click += new System.EventHandler(this.load_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 542);
+            this.Controls.Add(this.load);
+            this.Controls.Add(this.ispisLoads);
+            this.Controls.Add(this.saveime);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.splitIspis);
+            this.Controls.Add(this.split);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.ispis);
+            this.Name = "Form1";
+            this.Text = "Štoperica";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ispis;
+        private System.Windows.Forms.Button start;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button reset;
+        private System.Windows.Forms.Button split;
+        private System.Windows.Forms.RichTextBox splitIspis;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.TextBox saveime;
+        private System.Windows.Forms.ComboBox ispisLoads;
+        private System.Windows.Forms.Button load;
+    }
+}
+
---
+++ Stopwatch/Stoperica/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stoperica
+{
+    public partial class Form1 : Form
+    {
+        bool status = false;
+        int brojilo = 1;
+        int i = 0;
+        TimeSpan razlika;
+        TimeSpan temp;
+        TimeSpan tempSplit = TimeSpan.Zero;
+
+        string path = @"C:\Users\" + Environment.UserName + @"\stopericadata.txt";
+        List<string> stringLoads = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        public void Zapis()
+        {
+            stringLoads = File.ReadAllLines(path).ToList();
+            ispisLoads.Items.Clear();
+            int tempindex;
+            string tempime = "\t\n";
+            for (i = 0; i < stringLoads.Count; i++)
+            {
+                tempindex = stringLoads[i].IndexOf("\t");
+                tempime = stringLoads[i].Substring(0, tempindex);
+                tempime = tempime.Replace("\t", "");
+
+                ispisLoads.Items.Add(tempime);
+            }
+        }
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            backgroundWorker1.WorkerSupportsCancellation = true;
+            DateTime pocetno = DateTime.Now;
+            DateTime trenutacno;
+            while (!backgroundWorker1.CancellationPending)
+            {
+                trenutacno = DateTime.Now;
+                razlika = trenutacno - pocetno;
+                if (!(temp == null))
+                {
+                    razlika = temp + razlika;
+                }
+                ispis.Text = razlika.ToString();
+            }
+        }
+        private void start_Click(object sender, EventArgs e)
+        {
+            if (status == false)
+            {
+                backgroundWorker1.RunWorkerAsync();
+                start.Text = "Stop";
+                status = true;
+            }
+            else
+            {
+                backgroundWorker1.CancelAsync();
+                temp = razlika;
+                start.Text = "Start";
+                status = false;
+            }
+        }
+
+        private void split_Click(object sender, EventArgs e)
+        {
+            tempSplit = razlika - tempSplit;
+            splitIspis.Text = splitIspis.Text + "Split #" + brojilo + " Time: " + razlika + " Diff: " + tempSplit + "\n";
+            tempSplit = razlika;
+            brojilo++;
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.CancelAsync();
+            ispis.Text = "00:00:0000000";
+            splitIspis.Text = "";
+            tempSplit = TimeSpan.Zero;
+            temp = TimeSpan.Zero;
+            start.Text = "Start";
+            status = false;
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            if (saveime.Text != "Name of the save") {
+                string tempupis = saveime.Text + "\t" + ispis.Text + "\t" + splitIspis.Text.Replace("\n", "\t") + tempSplit + "\n";
+                File.AppendAllText(path, tempupis);
+            }
+        }
+
+        private void saveime_Enter(object sender, EventArgs e)
+        {
+            if (saveime.Text == "Name of the save")
+            {
+                saveime.Text = "";
+            }
+        }
+
+        private void saveime_Leave(object sender, EventArgs e)
+        {
+            if (saveime.Text == "")
+            {
+                saveime.Text = "Name of the save";
+            }
+        }
+
+        private void ispisLoads_Click(object sender, EventArgs e)
+        {
+            Zapis();
+        }
+
+        private void load_Click(object sender, EventArgs e)
+        {
+            string izabrano = ispisLoads.Text;
+            string saved;
+            string ime;
+            string splits;
+            int index;
+            int count;
+            TimeSpan vrijeme;
+            TimeSpan splitvrijeme;
+            for (i = 0; i < stringLoads.Count; i++)
+            {
+                if (stringLoads[i].Contains(izabrano))
+                {
+                    saved = stringLoads[i];
+                    index = saved.IndexOf("\t");
+                    ime = saved.Substring(0, index);
+                    ime = ime.Replace("\t", "");
+                    saved = saved.Replace(ime + "\t", "");
+
+
+                    index = saved.IndexOf("\t");
+                    vrijeme = TimeSpan.Parse(saved.Substring(0, index));
+                    temp = vrijeme;
+                    razlika = vrijeme;
+                    ispis.Text = vrijeme.ToString();
+                    saved = saved.Replace(Convert.ToString(vrijeme) + "\t", "");
+
+
+                    count = saved.Count(f => (f == 't'));
+                    brojilo = count + 1;
+
+                    index = saved.LastIndexOf("\t");
+                    splits = saved.Substring(0, index);
+                    saved = saved.Replace(splits, "");
+                    splits = splits.Replace("\n", "");
+                    splits = splits.Replace("\t", "\n");
+                    splitIspis.Text = splits + "\n";
+
+                    saved = saved.Replace("\n", "");
+                    saved = saved.Replace("\t", "");
+                    splitvrijeme = TimeSpan.Parse(saved.Substring(0, saved.Length));
+                    tempSplit = splitvrijeme;
+                }
+            }
+        }
+    }
+}
